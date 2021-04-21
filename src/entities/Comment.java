8d66832f1a685387f56e1bcf5640d3cab776daf6
Jonathan Reis <jonathan.linkedin2019@gmail.com>
2021-04-21 10:55:54 -0300

    Creating Comment and Post Classes

--- src/entities/Comment.java
+++ src/entities/Comment.java
@@ -2,4 +2,25 @@ package entities;
 
 public class Comment {
 
+	private String text;
+	
+	
+	public Comment() {
+		
+	}
+
+	public Comment(String text) {
+		this.text = text;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+	
+	
+	
 }
